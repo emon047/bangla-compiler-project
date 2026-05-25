0db871699e907b6f3cc10623041356021f3278c9
emon047 <emonelias550@gmail.com>
2026-05-25 20:33:59 +0600

    Add files via upload

---
+++ output.py
@@ -0,0 +1,30 @@
+# Auto-generated Python Target Code from Bangla Compiler
+
+প্রথম = 10
+দ্বিতীয় = 20
+যোগ = (প্রথম + দ্বিতীয়)
+print(যোগ)
+বিয়োগ = (প্রথম - দ্বিতীয়)
+print(বিয়োগ)
+গুণ = (প্রথম * দ্বিতীয়)
+print(গুণ)
+ভাগ = (প্রথম / দ্বিতীয়)
+print(ভাগ)
+if (যোগ > 150):
+    print(1)
+else:
+    print(0)
+লুপ = 1
+while (লুপ <= 3):
+    print(লুপ)
+    লুপ = (লুপ + 1)
+
+# Print final state of variables
+print('--- Execution Output ---')
+print('প্রথম:', প্রথম)
+print('দ্বিতীয়:', দ্বিতীয়)
+print('যোগ:', যোগ)
+print('বিয়োগ:', বিয়োগ)
+print('গুণ:', গুণ)
+print('ভাগ:', ভাগ)
+print('লুপ:', লুপ)
